a35f4c164dd5fff5e7be6afc79ec90c00c17e233
HIEU <hieu.n2395@gmail.com>
2017-12-22 14:32:08 +0700

    Commit 2

--- ZShop.Web/Properties/AssemblyInfo.cs
+++ ZShop.Web/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ef1a339b-c977-4995-937d-66aedb879e7d")]
+[assembly: Guid("a3e2c12d-bc68-48a3-b3b6-cfd18114c7d2")]
 
 // Version information for an assembly consists of the following four values:
 //
